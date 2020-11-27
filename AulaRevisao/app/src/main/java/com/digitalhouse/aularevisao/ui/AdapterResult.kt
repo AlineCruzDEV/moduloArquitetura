13887a38740bfe7e3d8b921882a9d46aeddd2fc1
AlineCruzDEV <linegcruz@gmail.com>
2020-11-27 20:56:55 -0300

    aula de revisao

---
+++ AulaRevisao/app/src/main/java/com/digitalhouse/aularevisao/ui/AdapterResult.kt
@@ -0,0 +1,39 @@
+package com.digitalhouse.aularevisao.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.digitalhouse.aularevisao.R
+import kotlinx.android.synthetic.main.item_result.view.*
+import java.util.ArrayList
+
+class AdapterResult(): RecyclerView.Adapter<AdapterResult.ResultViewHolder>() {
+
+    var listResult = ArrayList<String>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ResultViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_result, parent, false)
+        return ResultViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ResultViewHolder, position: Int) {
+        var item = listResult[position]
+        holder.tvResult.text = item
+    }
+
+    override fun getItemCount() = listResult.size
+
+    fun addList(list: ArrayList<String>){
+        listResult.addAll(list)
+        notifyDataSetChanged()
+    }
+
+    class ResultViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val tvResult: TextView = view.tvResult
+
+    }
+
+
+}
--- AulaRevisao/app/src/main/java/com/digitalhouse/aularevisao/ui/MainActivity.kt
+++ AulaRevisao/app/src/main/java/com/digitalhouse/aularevisao/ui/MainActivity.kt
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.digitalhouse.aularevisao.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapterResult: AdapterResult
+    lateinit var linearLayoutManager: LinearLayoutManager
 
     val viewModel: MainViewModel by viewModels()
 
@@ -14,11 +19,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        adapterResult = AdapterResult()
+        linearLayoutManager = LinearLayoutManager(this)
+        rcResult.adapter = adapterResult
+        rcResult.layoutManager = linearLayoutManager
+        rcResult.hasFixedSize()
+
         //atualizando valores da lista
         viewModel.popListResult()
 
         viewModel.listResults.observe(this){
+            adapterResult.addList(it)
             Log.i("MainActivity", it.toString())
         }
+
     }
 }
